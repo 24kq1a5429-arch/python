e7900749a9d401787958a519a2ca9b39dfc16778
24kq1a5429-arch <24kq1a5429@pace.ac.in>
2025-12-27 14:37:18 +0530

    odd-even.py

--- odd-even.py
+++ odd-even.py
@@ -1,5 +1,5 @@
 arr = list(map(int, input().split()))
 even = [x for x in arr  if  x % 2 == 0]
-odd = [x for x in arr if x % 2 == 0]
+odd = [x for x in arr if x % 2 != 0]
 result = even + odd
 print(result)
